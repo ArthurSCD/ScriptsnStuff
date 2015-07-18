586ad959ad25c41218b4bb9f4d917c9d40fcd0ba
LuciferIAm <blaze.7@live.com>
2015-07-18 16:54:30 -0500

    Update mysqldb.sh

--- mysqldb.sh
+++ mysqldb.sh
@@ -61,7 +61,7 @@ sed -i -e "s/localhost/$host/g" /wordpress/wp-config.php
 
 
 rsync -avpP /wordpress/ /var/www/$sitename/public/
-chown -R $ruser:www-data /var/www/"$sitename"/public/*
+chown -R $ruser:www-data /var/www/$sitename/public/*
 mkdir -p /var/www/$sitename/public/wp-content/uploads
 chown -R :www-data /var/www/$sitename/public/wp-content/uploads
 
