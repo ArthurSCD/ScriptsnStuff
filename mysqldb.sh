4372ff631be19844ce69e91609cd18902f6625af
LuciferIAm <blaze.7@live.com>
2015-07-18 02:34:41 -0500

    Update mysqldb.sh

--- mysqldb.sh
+++ mysqldb.sh
@@ -46,23 +46,23 @@ CREATE="${DB}${USN}${PWD}${FL};"
 
 mysql -e "$CREATE"
 
-cp ~/wordpress/wp-config-sample.php ~/wordpress/wp-config.php
-sed -i -e "s/database_name_here/$database/g" ~/wordpress/wp-config.php
-sed -i -e "s/username_here/$username/g" ~/wordpress/wp-config.php
-sed -i -e "s/password_here/$password/g" ~/wordpress/wp-config.php
-sed -i -e "s/localhost/$host/g" ~/wordpress/wp-config.php
+cp /home/"$ruser"/wordpress/wp-config-sample.php /home/"$ruser"/wordpress/wp-config.php
+sed -i -e "s/database_name_here/$database/g" /home/"$ruser"/wordpress/wp-config.php
+sed -i -e "s/username_here/$username/g" /home/"$ruser"/wordpress/wp-config.php
+sed -i -e "s/password_here/$password/g" /home/"$ruser"/wordpress/wp-config.php
+sed -i -e "s/localhost/$host/g" /home/"$ruser"/wordpress/wp-config.php
 
-mkdir -p "$sitename"
-rsync -avpP ~/wordpress/ "$sitename"
-chown -R "$ruser":www-data "$sitename"*
-mkdir "$sitename"wp-content/uploads
-chown -R :www-data "$sitename"wp-content/uploads
+mkdir -p /home/"$ruser"/var/www/"$sitename"/public
+rsync -avpP /home/"$ruser"/wordpress/ /home/"$ruser"/var/www/"$sitename"/public/
+chown -R "$ruser":www-data /home/"$ruser"/var/www/"$sitename"/public/*
+mkdir /home/"$ruser"/var/www/"$sitename"/public/wp-content/uploads
+chown -R :www-data /home/"$ruser"/var/www/"$sitename"/public/wp-content/uploads
 
 # nginx server blocks, must use provided default
 # wget https://raw.githubusercontent.com/LuciferIAm/ScriptsnStuff/master/autodef ~/etc/nginx/sites-available/
-cp ~/etc/nginx/sites-available/autodef ~/etc/nginx/sites-available/"$sitename"
-sed -i -e "s/example.com/$sitename/g" ~/etc/nginx/sites-available/"$sitename"
-ln -s ~/etc/nginx/sites-available/"$sitename" ~/etc/nginx/sites-enabled/
+cp /home/"$ruser"/etc/nginx/sites-available/autodef /home/"$ruser"/etc/nginx/sites-available/"$sitename"
+sed -i -e "s/example.com/$sitename/g" /home/"$ruser"/etc/nginx/sites-available/"$sitename"
+ln -s /home/"$ruser"/etc/nginx/sites-available/"$sitename" /home/"$ruser"/etc/nginx/sites-enabled/
 service nginx restart
 service php5-fpm restart
 
