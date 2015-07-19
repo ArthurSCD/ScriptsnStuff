611f384564ea232d644aceeb23e70ab6fec6b21f
LuciferIAm <blaze.7@live.com>
2015-07-19 16:28:46 -0500

    Update wpinstall.sh

--- wpinstall.sh
+++ wpinstall.sh
@@ -13,10 +13,10 @@ sed -i -e "s/password_here/$password/g" "/home/$ruser/wordpress/wp-config.php"
 
 
 
-rsync -avpP "/home/$ruser/wordpress/" "/var/www/$sitename/public/"
-chown -R $ruser:www-data "/var/www/$sitename/public/*"
-mkdir -p "/var/www/$sitename/public/wp-content/uploads"
-chown -R :www-data "/var/www/$sitename/public/wp-content/uploads"
+rsync -avpP "/home/$ruser/wordpress/" "/srv/www/$sitename/public/"
+chown -R $ruser:www-data "/srv/www/$sitename/public/*"
+mkdir -p "/srv/www/$sitename/public/wp-content/uploads"
+chown -R :www-data "/srv/www/$sitename/public/wp-content/uploads"
 
 # nginx server blocks, must use provided default
 # wget https://raw.githubusercontent.com/LuciferIAm/ScriptsnStuff/master/autodef ~/etc/nginx/sites-available/
