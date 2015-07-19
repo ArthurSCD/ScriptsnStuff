f47fd8c50db51064419447e6fd2eadbcfd97cff7
LuciferIAm <blaze.7@live.com>
2015-07-19 11:22:07 -0500

    Update wpinstall.sh

--- wpinstall.sh
+++ wpinstall.sh
@@ -10,7 +10,7 @@ cp "/home/$ruser/wordpress/wp-config-sample.php" "/home/$ruser/wordpress/wp-conf
 sed -i -e "s/database_name_here/$database/g" "/home/$ruser/wordpress/wp-config.php"
 sed -i -e "s/username_here/$username/g" "/home/$ruser/wordpress/wp-config.php"
 sed -i -e "s/password_here/$password/g" "/home/$ruser/wordpress/wp-config.php"
-sed -i -e "s/localhost/$host/g" "/home/$ruser/wordpress/wp-config.php"
+
 
 
 rsync -avpP "/home/$ruser/wordpress/" "/var/www/$sitename/public/"
