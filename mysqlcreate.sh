02bf851021e9e3f2a385456f0415bd3f184fb0c2
LuciferIAm <blaze.7@live.com>
2015-07-19 11:26:24 -0500

    Create mysqlcreate.sh

---
+++ mysqlcreate.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+if [ $(id -u) -ne 0 ]
+ then
+    sudo $0
+    exit 0
+fi
+
+if [ $(uname) = "Linux" ] 
+  then
+
+	while [ -z "$database" ];
+	do
+		echo -n "Enter name of database: "
+		read database
+	done
+
+	while [ -z "$username" ];
+	do
+		echo -n "Enter username: "
+		read username
+	done
+	
+	while [ -z "$password" ];
+	do
+		echo -n "Enter password: "
+		read password
+	done
+	
+	while [ -z "$sitename" ];
+	do
+		echo -n "Enter sitename: "
+		read sitename
+	done
+
+	while [ -z "$ruser" ];
+	do
+		echo -n "Root user (you): "
+		read ruser
+	done		
+fi
+
+DB="CREATE DATABASE $database;"
+USN="CREATE USER $username@localhost IDENTIFIED BY '$password';"
+PWD="GRANT ALL PRIVILEGES ON $database.* TO $username@localhost;"
+FL="FLUSH PRIVILEGES;"
+CREATE="${DB}${USN}${PWD}${FL};"
+
+mysql -e "$CREATE"
+
+export database
+export username
+export password
+export sitename
+export ruser
+
+./wpinstall.sh
